619cf835e12497befadddd3d952ec89ec0404d94
Dan Kelley <dantheflipman@gmail.com>
2017-11-15 11:42:03 -0500

    Added progress bar / updated with more comments / explanations,cleaned up some redundant code

--- FindPlayers.ps1
+++ FindPlayers.ps1
@@ -1,14 +1,31 @@
 ﻿
 #Echo arena log scraper -- DAK 20171114
 
+#Adding the windows forms assembly so we can show the folder browser, and save menu
 Add-Type -AssemblyName System.Windows.Forms
+
+#Create a new folder browser instance
 $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
 [void]$FolderBrowser.ShowDialog()
 $LogDirectory = $FolderBrowser.SelectedPath
 
-$Logs = gci -Path $LogDirectory -Filter "*.log" | Select Name -ExpandProperty "Name";
+#get-childItem returns all files in a directory; we're only interested in the name of "*.log" files though
+$Logs = Get-ChildItem -Path $LogDirectory -Filter "*.log" | Select Name -ExpandProperty "Name"; 
+
+
 $NameMatch = [Regex]::new("^\[(\d{1,2})-(\d{1,2})-(\d{4})\]\s\[(\d{1,2}):(\d{1,2}):(\d{1,2})\].*?\[NETGAME\]:\s{2}Name\s+?:\s(.+)$");
 
+<# essential regex pieces
+
+^                                     = Beginning of a line 
+\[(\d{1,2})-(\d{1,2})-(\d{4})\]       = looks for a date, enclosed in brackets, in MM-DD-YYYY format. Capture groups 1 - 3
+\[(\d{1,2}):(\d{1,2}):(\d{1,2})\]     = looks for a time, enclosed in brackets, in HH:MM:SS format. Capture groups 4 - 6
+\[NETGAME\]:\s{2}                     = looks for the string "[NETGAME]" followed by a colon ":" and two spaces "\s{2}"
+Name\s+?:                             = looks for the string "Name" followed by any number of spaces \s+? then a colon ":"
+\s(.+)                                = looks for one space "\s", then captures every character after "(.+)" (Capture group 7)
+$                                     = End of the line
+
+#>
 
 #Create Table object
 $tableName = "Players"
@@ -23,31 +40,40 @@ $table.columns.add($dateCol)
 $table.columns.add($nameCol)
 
 
+$logIndex = 1; #Let's keep track while enumerating through the list of logs
 
 Foreach ($log in $Logs)
 {
+    #write what we're doing to the standard output
     Write-Output "";
     Write-Output "Scanning $Log";
+
+    #log filenames have abnormal chars in them, so we're using -LiteralPath instead of Path
     $LogContents = get-content -LiteralPath "$LogDirectory\$log";
 
-    $lineIndex = 0; #keep track of where we are numerically just in case we need to use this
+    #Also keep track of where we are within each log file
+    $lineIndex = 0; 
+
+    #Build Status and percentage for the progress bar
+    $Status = "Scanning Log " + ($logIndex) + " of " + ($Logs.Count)
+    $PercentComplete = ($logIndex / $Logs.count*100)
+
+    #Show progress bar
+    Write-Progress -Activity "Scanning Logs" -status $Status -percentComplete $PercentComplete
 
     foreach ($line in $LogContents)
     {
         
         if ($NameMatch.IsMatch($line) -and $LogContents[$lineIndex-1] -notlike "*disconnected*")
         {
-            
-            $day = $line -replace $NameMatch,"`$1"
-            $month = $line -replace $NameMatch,"`$2"
-            $year = $line -replace $NameMatch,"`$3"
-            $HH = $line -replace $NameMatch,"`$4"
-            $MM = $line -replace $NameMatch,"`$5"
-            $SS = $line -replace $NameMatch,"`$6"
+            #Create a usable datetime object from our regex match groups 1-6
+            $dateTimeString = $line -replace $NameMatch,"`$1/`$2/`$3 `$4:`$5:`$6"
+            $date = [DateTime]$dateTimeString
 
-            $date = [DateTime]"$day/$month/$year $HH`:$MM`:$SS"
-            $name = $line -replace $NameMatch,"`$7"
+            #Get our player name from regex match group 7
+            $name = $line -replace $NameMatch,"`$7" 
 
+            #Add that information into a new row, then add that row to our table
             $row = $table.NewRow()
             $row.DateConnected = $date
             $row.PlayerName = $name
@@ -57,9 +83,10 @@ Foreach ($log in $Logs)
         }
         $lineIndex ++;
     }
-
+    $logIndex ++;
 }
 
+#If there are entries in our table, save them
 if ($table.Rows.Count -gt 0) 
 {
  
@@ -69,7 +96,14 @@ if ($table.Rows.Count -gt 0)
     $SaveFileDialog.AddExtension = $true
     $SaveFileDialog.DefaultExt = "csv"
     $SaveFileDialog.filename = "PlayerNames.csv"
-    $SaveFileDialog.ShowDialog() |  Out-Null
-    
-    $table | Export-Csv -Path $SaveFileDialog.filename -NoTypeInformation 
+    if ($SaveFileDialog.ShowDialog() -ne "Cancel") 
+    {
+        Write-Output ("Saving output to : " + $SaveFileDialog.filename)
+        $table | Export-Csv -Path $SaveFileDialog.filename -NoTypeInformation
+        Write-Output "Save complete. Closing"
+    }
+    else 
+    {
+        Write-Output "Save cancelled. Closing"
+    }
 }
